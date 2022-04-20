866c55af0861e8b4bfa5fac05d147415e039f1a0
leiy-zhangt <1372872918@qq.com>
2022-04-20 16:29:01 +0800

    修改了delay函数

--- User/main.c
+++ User/main.c
@@ -11,14 +11,13 @@ int main(void)
     RCC_Configuration();
     NVIC_Configuration();
 	SPI1_Configuration();
-    delay_init(168);
     LED_Init();
     uart_init(115200);
     while(1)
     {
         USART_SendData(USART1,'a');
         LED=!LED;
-        delay_ms(1000);
+        delay_ms(65000);
     }
 }
 
